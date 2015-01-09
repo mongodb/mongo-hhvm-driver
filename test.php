cba1ba0bf69de2f09c5317d6e082ecd9719025f7
Derick Rethans <github@derickrethans.nl>
2015-01-09 11:21:49 +0000

    Added other datatypes to test file.

--- test.php
+++ test.php
@@ -6,6 +6,10 @@ $m = new MongoDB\Manager("mongodb://localhost:27017");
 $doc = [
 	'name' => 'Derick',
 	'twitter' => 'derickr',
+	'age' => 36.1,
+	'length' => 185,
+	'does_opensource' => true,
+	'null_value' => null,
 	'site' => 'http://derickretans.nl',
 ];
 
