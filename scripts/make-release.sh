7070d5b8b4a42bddc71c4bca4285851090788961
Derick Rethans <github@derickrethans.nl>
2015-09-24 15:03:00 +0100

    We can't remove things from here, as that's what the autogens expect

--- scripts/make-release.sh
+++ scripts/make-release.sh
@@ -5,21 +5,14 @@ version=$1
 rm -rf /tmp/hhvm-build
 mkdir -p /tmp/hhvm-build
 cd /tmp/hhvm-build
-git clone git@github.com:mongodb-labs/mongo-hhvm-driver-prototype.git hhvm-mongo-${version}
-cd hhvm-mongo-${version}
+git clone git@github.com:mongodb-labs/mongo-hhvm-driver-prototype.git hhvm-mongodb-${version}
+cd hhvm-mongodb-${version}
 git checkout "$version"
 git submodule init
 git submodule update
 
 #removing nonsense
-rm -rf /tmp/hhvm-build/hhvm-mongo-${version}/.git
-rm -rf /tmp/hhvm-build/hhvm-mongo-${version}/libbson/doc
-rm -rf /tmp/hhvm-build/hhvm-mongo-${version}/libbson/tests
-rm -rf /tmp/hhvm-build/hhvm-mongo-${version}/libmongoc/doc
-rm -rf /tmp/hhvm-build/hhvm-mongo-${version}/libmongoc/examples
-rm -rf /tmp/hhvm-build/hhvm-mongo-${version}/libmongoc/orchestration_configs
-
-
+rm -rf /tmp/hhvm-build/hhvm-mongodb-${version}/.git
 
 cd -
-tar cvzf /tmp/hhvm-mongodb-${version}.tgz hhvm-mongo-${version}
+tar cvzf /tmp/hhvm-mongodb-${version}.tgz hhvm-mongodb-${version}
